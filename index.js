68f367ba6c791e1978299807826cb1e8c195b31b
Venky Donthula <venkateshdonthula2002@gmail.com>
2024-02-18 09:45:47 +0530

    aasdsad

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express")
 const app = express();
+const nodemailer = require("nodemailer");
 const cors = require("cors")
 app.use(express.json())
 app.use(cors())
 app.get("/", (req, res) => {
     res.send("Welcome to the VARCBYTES CICD Pipeline")
+})
+app.get("/ping", (
+    req, res
+) => {
+    var nm_inst = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'venkateshdonthula@varcsoft.com',
+            pass: 'Venky@0308'
+        }
+    });
+    var mailOptions = {
+        from: 'venkateshdonthula@varcsoft.com',
+        to: 'robot@varcsoft.com',
+        subject: 'CICD - VARCBYTES',
+        text: 'That was easy!'
+    };
+    nm_inst.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            res.send("Error")
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send("Email sent")
+        }
+    });
 })
 app.listen(5000, () => {
     console.log("Server is running on port 5000")
