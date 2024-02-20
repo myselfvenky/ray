8b8b0ccbc86ae930294b0090748da5249e4754c7
Venky Donthula <venkateshdonthula2002@gmail.com>
2024-02-20 08:18:13 +0530

    adadas

---
+++ Controllers/EmailService/SendEmail.js
@@ -0,0 +1,36 @@
+import nodemailer from "nodemailer";
+export const sendEmail = async (from, to, subject, text, html) => {
+
+    var nm_inst = nodemailer.createTransport({
+        host: 'box.varcsoft.com',
+        port: 465,
+        auth: {
+            user: 'venkateshdonthula@varcsoft.com',
+            pass: 'venky@0308'
+        }
+    });
+
+    const textBody = {
+        text: text,
+    }
+    const htmlBody = {
+        html: html,
+    }
+    const finalBody = html ? htmlBody : textBody
+
+    var mailOptions = {
+        from: from,
+        to: to,
+        subject: 'CICD - VARCBYTES',
+        ...finalBody
+    };
+    nm_inst.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            return error
+        } else {
+            console.log('Email sent: ' + info.response);
+            return info.response
+        }
+    });
+}
--- index.js
+++ index.js
@@ -1,41 +1,85 @@
-const express = require("express")
+import express from "express";
 const app = express();
-const nodemailer = require("nodemailer");
-const cors = require("cors")
+import dotenv from "dotenv"
+import cors from "cors"
+import { sendEmail } from "./Controllers/EmailService/SendEmail.js";
+import { initprisma, prisma } from "./Initalizer/initPrisma.js";
 app.use(express.json())
 app.use(cors())
+dotenv.config()
+initprisma()
 app.get("/", (req, res) => {
     res.send("Welcome to the VARCBYTES CICD Pipeline")
 })
+app.get("/getPorts", async (req, res) => {
+    try {
+        const result = await prisma.ports.findMany()
+        if (result) {
+            res.send(result)
+            return
+        }
+        res.send("Error")
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err)
+    }
+})
+app.post("/createPort", async (req, res) => {
+    try {
+
+        const data = req.body.config;
+        if (!data) {
+            res.status(400).send("Invalid Data")
+            return
+        }
+        if (data.port === undefined || data.port === null) {
+            res.status(400).send("Invalid Port")
+            return
+        }
+        const port = await prisma.ports.findUnique({
+            where: {
+                port: data.port
+            }
+        });
+        if (port) {
+            res.status(400).send("Port Already Exists")
+            return
+        }
+        const result = await prisma.ports.create({
+            data: data
+        })
+        if (result) {
+            res.send(result)
+            return
+        }
+        res.send("Error")
+    } catch (err) {
+        console.log(err)
+        res.send(err)
+    }
+
+})
+
 
 app.post("/ping", (
     req, res
 ) => {
+    console.log(res)
+    res.send(sendEmail(
+        "venkateshdonthula@varcsoft.com",
+        // eslint-disable-next-line no-undef
+        process.env.TO_EMAIL,
+        "CICD - VARCBYTES",
+        "That was easy!",
+        "That was easy!"
+    ))
 
-    var nm_inst = nodemailer.createTransport({
-        host: 'box.varcsoft.com',
-        port: 465,
-        auth: {
-            user: 'venkateshdonthula@varcsoft.com',
-            pass: 'venky@0308'
-        }
-    });
-    var mailOptions = {
-        from: 'venkateshdonthula@varcsoft.com',
-        to: 'robot@varcsoft.com',
-        subject: 'CICD - VARCBYTES',
-        text: 'That was easy!'
-    };
-    nm_inst.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            res.send("Error")
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.send("Email sent")
-        }
-    });
 })
 app.listen(5000, () => {
-    console.log("Server is running on port 5000")
+    // eslint-disable-next-line no-undef
+    process.env.NODE_ENV === "development" ?
+        // eslint-disable-next-line no-undef
+        console.log(`Server is running on ${process.env.DEV_URL}:{process.env.DEV_PORT}`) :
+        // eslint-disable-next-line no-undef
+        console.log(`Server is running on ${process.env.PROD_URL}:${process.env.PROD_PORT}`)
 })
